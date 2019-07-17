b92536a27c6ba0ccaf9ba4d84572319b9eb8f2b2
Raul Gavris <rg.raulgavris@gmail.com>
2019-07-17 13:24:07 +0300

    wifi working but not NetworkManager, to do! NetworkManager!

--- pre_rg.ai_2.sh
+++ pre_rg.ai_2.sh
@@ -19,39 +19,44 @@ echo "127.0.1.1       rg.localdomain  rg" >> /etc/hosts
 
 pacman -S --noconfirm --needed dialog sudo zsh os-prober
 
-pacman --noconfirm --needed -S wpa_supplicant
-pacman --noconfirm --needed -S wireless_tools
-pacman --noconfirm --needed -S networkmanager
-pacman --noconfirm --needed -S network-manager-applet
-pacman --noconfirm --needed -S gnome-keyring
+pacman -S --noconfirm --needed ifplugd wpa_supplicant
 
-systemctl enable NetworkManager.service
+#pacman --noconfirm --needed -S wpa_supplicant
+#pacman --noconfirm --needed -S wireless_tools
+#pacman --noconfirm --needed -S networkmanager
+#pacman --noconfirm --needed -S network-manager-applet
+#pacman --noconfirm --needed -S gnome-keyring
 
-systemctl enable wpa_supplicant.service
+#systemctl enable NetworkManager.service
 
-# gpasswd -a rg network
+#systemctl enable wpa_supplicant.service
 
-ip link set down lo
-ip link set down enp4s0f1
-ip link set down wlp3s0
+#gpasswd -a rg network
 
-systemctl start wpa_supplicant.service
+#ip link set down lo
+#ip link set down enp4s0f1
+#ip link set down wlp3s0
 
-systemctl disable dhcpcd.service
-systemctl disable dhcpcd@.service
-systemctl stop dhcpcd.service
-systemctl stop dhcpcd@.service
+#systemctl start wpa_supplicant.service
 
-systemctl start NetworkManager.service
+#systemctl disable dhcpcd.service
+#systemctl disable dhcpcd@.service
+#systemctl stop dhcpcd.service
+#systemctl stop dhcpcd@.service
 
-dialog --no-cancel --inputbox "Enter password for root." 10 65 2>rootpasswd
+#systemctl start NetworkManager.service
 
-cat <<EOF | passwd
-	${rootpasswd}
-	${rootpasswd}
-EOF
+#dialog --no-cancel --inputbox "Enter password for root." 10 65 2>rootpasswd
+
+#cat <<EOF | passwd
+#	${rootpasswd}
+#	${rootpasswd}
+#EOF
 
 pacman --noconfirm --needed -S grub && grub-install --target=i386-pc --recheck /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
+
+useradd -m -G wheel -s /usr/bin/zsh raulgavris
+chsh -s /usr/bin/zsh
 
 pacman -Sy --noconfirm
 pacman -Su --noconfirm
@@ -61,6 +66,12 @@ dialog --title "Install rg.ai" --yesno "Would you like to go on and install rg.a
 dialog --defaultno --title "Finally, here\! All installed\!" --yesno "Reboot computer?"  5 30 && reboot
 clear
 
+echo "visudo -> %wheel ALL uncomment"
+echo "passwd"
+echo "passwd user"
+
+echo "systemctl enable netctl-auto@wlp..."
+echo "systemctl enable netctl-ifplugd@enp..."
 
 #################################################################
 # ln -sf /etc/profile ~/.profile # for startx to start everytime
